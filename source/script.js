fb340829cd6d315fb02714fe807a749628ebe7bc
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2024-11-24 18:54:39 +0200

    defined a function to search a city

--- source/script.js
+++ source/script.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
